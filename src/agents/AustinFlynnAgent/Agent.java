5d4aa0e1aba68bc039ced061c27f7383e8734eec
Emily Austin <bamboee@sbcglobal.net>
2021-12-01 22:17:23 -0500

    added utility for wrath

--- src/agents/AustinFlynnAgent/Agent.java
+++ src/agents/AustinFlynnAgent/Agent.java
@@ -9,13 +9,18 @@ public class Agent implements MarioAgent {
     private enum mood  {
         COLLECT, KILL, PROGRESS
     }
+
     private mood currentMood;
     private int sense_radius = 5;
+
     private float[] getEnemies(MarioForwardModel model){
         return model.getEnemiesFloatPos();
     }
+
     private float getModelX(MarioForwardModel model){return model.getMarioFloatPos()[0];}
+
     private float getModelY(MarioForwardModel model){return model.getMarioFloatPos()[1];}
+
     private boolean enemiesNear(MarioForwardModel model){
 
         for(int e =0; e<getEnemies(model).length;e++) {
@@ -25,20 +30,16 @@ public class Agent implements MarioAgent {
         }
         return false;
     }
+
     private float getDistance(MarioForwardModel model, float x, float y){
         return (float)Math.sqrt(((getModelX(model)-x) * (getModelX(model)-x) + ((getModelY(model)-y) * (getModelY(model)-y))));
     }
+
     private void reassessMood(MarioForwardModel model){
-        float wrath = 0; //urge to kill
+        float wrath = enemyNearUtility(model); //urge to kill
         float greed = 0; //urge to collect
         float temperance = 10; //urge to progress
-        if(enemiesNear(model)){
-            for(int e =0; e<getEnemies(model).length;e++) {
-                if (getDistance(model, getEnemies(model)[(3 * e) + 1], getEnemies(model)[(3 * e) + 2]) <= sense_radius) {
-                    wrath += 12-getDistance(model, getEnemies(model)[(3 * e) + 1], getEnemies(model)[(3 * e) + 2]);
-                }
-            }
-        }
+
         if(wrath > greed && wrath > temperance){
             currentMood = mood.KILL;
         }
@@ -47,6 +48,21 @@ public class Agent implements MarioAgent {
         }
         else currentMood = mood.PROGRESS;
     }
+
+    private float enemyNearUtility(MarioForwardModel model){
+        float utility = 0;
+        float numEnemies = 0;
+        if(enemiesNear(model)){
+            for(int e =0; e<getEnemies(model).length;e++) {
+                if (getDistance(model, getEnemies(model)[(3 * e) + 1], getEnemies(model)[(3 * e) + 2]) <= sense_radius) {
+                    utility += getDistance(model, getEnemies(model)[(3 * e) + 1], getEnemies(model)[(3 * e) + 2]);
+                    numEnemies++;
+                }
+            }
+        }
+        return numEnemies/utility;
+    }
+
     @Override
     public void initialize(MarioForwardModel model, MarioTimer timer){
         currentMood = mood.PROGRESS;
