e8243763329c5ae020528f05936f5b683c7460ff
Jimmy Flynn <jflynn2@wpi.edu>
2021-12-10 13:15:43 -0500

    SUS SUS AMOGUS

--- src/PlayLevel.java
+++ src/PlayLevel.java
@@ -34,6 +34,6 @@ public class PlayLevel {
     public static void main(String[] args) {
         MarioGame game = new MarioGame();
         //printResults(game.playGame(getLevel("./levels/original/lvl-1.txt"), 200, 0));
-        printResults(game.runGame(new agents.AustinFlynnAgent.Agent(), getLevel("./levels/ge/lvl-999.txt"), 20, 0, true));
+        printResults(game.runGame(new agents.AustinFlynnAgent.Agent(), getLevel("./levels/hopper/lvl-1.txt"), 20, 0, true));
     }
 }
--- src/agents/AustinFlynnAgent/Agent.java
+++ src/agents/AustinFlynnAgent/Agent.java
@@ -100,39 +100,60 @@ public class Agent implements MarioAgent {
 
                 float nearestEnemyX = 0;
                 float nearestEnemyY = 0;
+                int nearestEnemyIndex = 0;
                 boolean isSpiky = false;
+                boolean isWinged = false;
                 for(int e=0; e<getEnemies(model).length/3; e++){
                     if(e==0){
-                        if(getEnemies(model)[3*e] == 8){
-                            isSpiky = true;
-                        }
+
                         nearestEnemyX = getEnemies(model)[(3*e)+1];
                         nearestEnemyY = getEnemies(model)[(3*e)+2];
                     }
                     else if(getDistance(model, getEnemies(model)[(3*e)+1], getEnemies(model)[(3*e)+2])<getDistance(model,nearestEnemyX, nearestEnemyY)){
                         nearestEnemyX = getEnemies(model)[(3*e)+1];
                         nearestEnemyY = getEnemies(model)[(3*e)+2];
+                        nearestEnemyIndex = e;
                     }
+                }
+                if(getEnemies(model)[3*nearestEnemyIndex] == 8){
+                    isSpiky = true;
+                }
+                if(getEnemies(model)[3*nearestEnemyIndex] == 3f || getEnemies(model)[3*nearestEnemyIndex] == 5f ||
+                        getEnemies(model)[3*nearestEnemyIndex] == 7f || getEnemies(model)[3*nearestEnemyIndex] == 9f){
+                    isWinged = true;
                 }
                    if(model.getMarioMode() == 2 && nearestEnemyY == model.getMarioFloatPos()[1]){
                        actions[MarioActions.SPEED.getValue()] = true;
                    }
                    else if(getDistance(model, nearestEnemyX,nearestEnemyY)<45){
-                       if(!isSpiky){
+                       if(!isSpiky && !isWinged){
+                           actions[MarioActions.SPEED.getValue()] = false;
                            actions[MarioActions.JUMP.getValue()] = true;
                            if(model.getMarioFloatPos()[0]<nearestEnemyX){
-                               actions[MarioActions.RIGHT.getValue()] = false;
+                               actions[MarioActions.LEFT.getValue()] = false;
+                               actions[MarioActions.RIGHT.getValue()] = true;
                            }
                            else if (model.getMarioFloatPos()[0]>nearestEnemyX){
+                               actions[MarioActions.RIGHT.getValue()] = false;
                                actions[MarioActions.LEFT.getValue()] = true;
                            }
                        }
-                       else if(getDistance(model, nearestEnemyX,nearestEnemyY)<30 && model.getMarioFloatPos()[0]<nearestEnemyX){
+                       else if(getDistance(model, nearestEnemyX,nearestEnemyY)<30 && model.getMarioFloatPos()[0]<nearestEnemyX && isSpiky){
                            actions[MarioActions.JUMP.getValue()] = true;
                            actions[MarioActions.RIGHT.getValue()] = true;
                        }
-                       else{
-                           actions[MarioActions.RIGHT.getValue()] = true;
+                       else if(isWinged){
+                           if(nearestEnemyY == model.getMarioFloatPos()[1]){
+                               if(model.getMarioFloatPos()[0]<nearestEnemyX){
+                                   actions[MarioActions.RIGHT.getValue()] = true;
+                               }
+                               else if (model.getMarioFloatPos()[0]>nearestEnemyX){
+                                   actions[MarioActions.LEFT.getValue()] = true;
+                               }
+                           }
+                           else{
+                               actions[MarioActions.RIGHT.getValue()] = true;
+                           }
                        }
                    }
                    break;
